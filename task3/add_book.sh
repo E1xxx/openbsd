09399936b7ec7436af108b61f8fdbd6f301183f5
E1xxx <121410482+E1xxx@users.noreply.github.com>
2026-04-05 15:21:43 +0300

    Update add_book.sh

--- task3/add_book.sh
+++ task3/add_book.sh
@@ -1,143 +1,68 @@
 #!/bin/sh
 
-set -e
-
-trap 'echo "Error: Script failed at line $LINENO" >&2; exit 1' ERR
-
 usage() {
-    for msg in "$@"; do printf "%s\n" "${0##*/}: $msg" >&2; done
-    echo "usage: ${0##*/} [-v] book_file book_name price author ..." >&2
-    exit 1
+	for msg in "$@"; do printf "%s\n" "${0##*/}: $msg" >&2; done
+	echo "usage:${0##*/} content_file name_file price authors" >&2
+	exit 1
 }
 
+set -e
+trap 'echo "Error occured"' ERR
 
-VERBOSE=0
-while getopts "v" opt; do
-    case $opt in
-        v) VERBOSE=1 ;;
-        *) usage "invalid option" ;;
-    esac
-done
-shift $((OPTIND-1))
+if [ $# -lt 4 ]; then
+	usage "missing arguments"
+fi
 
-test $# -ge 4 || usage "not enough arguments"
+content_file="$1"
+name_file="$2"
+price="$3"
 
-BOOK_FILE="$1"
-BOOK_NAME="$2"
-PRICE="$3"
 shift 3
-AUTHORS="$@"
 
+lib_root=$(pwd)
 
-if [ ! -f "$BOOK_FILE" ]; then
-    echo "${0##*/}: book file '$BOOK_FILE' does not exist" >&2
-    exit 1
-fi
-
-
-find_library_root() {
-    local dir="$1"
-    while [ "$dir" != "/" ]; do
-        if [ -f "$dir/.library" ]; then
-            if [ "$VERBOSE" -eq 1 ]; then
-                echo "Found library root at: $dir" >&2
-            fi
-            echo "$dir"
-            return 0
-        fi
-        dir=$(dirname "$dir")
-    done
-    echo "${0##*/}: not in a library directory (no .library marker found)" >&2
-    exit 1
-}
-
-
-CURRENT_DIR=$(pwd)
-LIBRARY_ROOT=$(find_library_root "$CURRENT_DIR")
-
-# Определение путей
-BOOKS_DIR="$LIBRARY_ROOT/books"
-AUTHORS_DIR="$LIBRARY_ROOT/authors"
-PRICES_DIR="$LIBRARY_ROOT/prices"
-TMP_DIR="$LIBRARY_ROOT/.tmp"
-
-# Проверка существования необходимых каталогов
-for dir in "$BOOKS_DIR" "$AUTHORS_DIR" "$PRICES_DIR" "$TMP_DIR"; do
-    if [ ! -d "$dir" ]; then
-        echo "${0##*/}: required directory '$dir' does not exist" >&2
-        exit 1
-    fi
+while [ "$lib_root" != "/" ] && [ ! -f "$lib_root/.library" ]; do
+	lib_root=$(dirname "$lib_root")
 done
 
-
-find_unique_filename() {
-    local base_name="$1"
-    local suffix=""
-    local counter=1
-    
-    while [ -e "$BOOKS_DIR/${base_name}${suffix}" ]; do
-        suffix=".$counter"
-        counter=$((counter + 1))
-    done
-    echo "${base_name}${suffix}"
-}
-
-
-FINAL_BOOK_NAME=$(find_unique_filename "$BOOK_NAME")
-FINAL_BOOK_PATH="$BOOKS_DIR/$FINAL_BOOK_NAME"
-
-if [ "$VERBOSE" -eq 1 ]; then
-    echo "Final book name: $FINAL_BOOK_NAME" >&2
+if [ -z "$lib_root" ] || [ ! -f "$lib_root/.library" ]; then
+	echo "Cannot find library root dir"
+	false
 fi
 
-
-TMP_BOOK_NAME="tmp_book_$$_$(date +%s%N)"
-TMP_BOOK_PATH="$TMP_DIR/$TMP_BOOK_NAME"
-
-if [ "$VERBOSE" -eq 1 ]; then
-    echo "Copying to tmp: $TMP_BOOK_PATH" >&2
+if [ ! -f "$content_file" ]; then
+	echo "Cannot find content_file"
+	false
 fi
 
-cp "$BOOK_FILE" "$TMP_BOOK_PATH" || {
-    echo "${0##*/}: failed to copy book file to temp directory" >&2
-    exit 1
-}
+suff=""
+cnt=1
 
-
-mv "$TMP_BOOK_PATH" "$FINAL_BOOK_PATH"
-
-
-chmod 664 "$FINAL_BOOK_PATH"
-
-
-if [ -n "$PRICE" ] && [ "$PRICE" != "-" ]; then
-    PRICE_FILE="$PRICES_DIR/${FINAL_BOOK_NAME}.price"
-    echo "$PRICE" > "$PRICE_FILE"
-    chmod 644 "$PRICE_FILE" 
-    if [ "$VERBOSE" -eq 1 ]; then
-        echo "Created price file: $PRICE_FILE" >&2
-    fi
-fi
-
-
-for author in $AUTHORS; do
-    AUTHOR_DIR="$AUTHORS_DIR/$author"
-    
-
-    if [ ! -d "$AUTHOR_DIR" ]; then
-        mkdir "$AUTHOR_DIR"
-        AUTHOR_DIR_PERMS=$(stat -c "%a" "$AUTHORS_DIR")
-        chmod "$AUTHOR_DIR_PERMS" "$AUTHOR_DIR"
-    fi
-    
-    AUTHOR_BOOK_LINK="$AUTHOR_DIR/$FINAL_BOOK_NAME"
-    ln "$FINAL_BOOK_PATH" "$AUTHOR_BOOK_LINK"
-    
-    if [ "$VERBOSE" -eq 1 ]; then
-        echo "Created link for author '$author': $AUTHOR_BOOK_LINK" >&2
-    fi
+while [ -e "$lib_root/books/${name_file}${suff}" ]; do
+	suff=".$cnt"
+	cnt=$((cnt+1))
 done
 
-if [ "$VERBOSE" -eq 1 ]; then
-    echo "Book '$BOOK_NAME' added successfully as '$FINAL_BOOK_NAME'" >&2
+name_file="${name_file}${suff}"
+
+tmp_file="$lib_root/.tmp/${name_file}.tmp"
+cp "$content_file" "$tmp_file"
+
+mv "$tmp_file" "$lib_root/books/$name_file"
+chmod 664 "$lib_root/books/$name_file"
+
+if [ -n "$price" ] && [ "$price" != "-" ]; then
+	echo "$price" > "$lib_root/prices/${name_file}.price"
+	chmod 644 "$lib_root/prices/${name_file}.price"
 fi
+
+for author in "$@"; do
+	a_dir="$lib_root/authors/$author"
+	if [ ! -d "$a_dir" ]; then
+        	mkdir "$a_dir"
+        	chmod 755 "$a_dir"
+	fi
+	
+	ln -f "$lib_root/books/$name_file" "$a_dir/$name_file"
+
+done
